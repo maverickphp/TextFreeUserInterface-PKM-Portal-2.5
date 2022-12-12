fbddfd18b2621513e50bff6810faf1b1c11072cf
Muhammad Qasim <m.qasim162000@gmail.com>
2022-12-12 20:53:40 +0500

    footer a tag fixed

--- src/components/UI/Footer.js
+++ src/components/UI/Footer.js
@@ -5,11 +5,16 @@ export default function Footer() {
     <footer className="text-center text-lg-start bg-light">
       <div
         className="text-center p-3 text-white"
-        style={{ backgroundColor: "rgb(23 43 81 / 1)" , height: "158px"}}
+        style={{ backgroundColor: "rgb(23 43 81 / 1)", height: "158px" }}
       >
         © 2020 Copyright &nbsp;
-        <br></br><a className="text-white" href="!#">
-          Maverickphp.com - Devnigma.com
+        <br></br>
+        <a className="text-white" href="https://maverickphp.com">
+          Maverickphp.com
+        </a>
+        &nbsp; -&nbsp;
+        <a className="text-white" href="https://devnigma.com">
+          Devnigma.com
         </a>
       </div>
     </footer>
